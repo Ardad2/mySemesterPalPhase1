62166f4bbc361600ce47efdc4ed9cc1b52387a4f
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-03-18 03:38:24 -0700

    created course dictionary with list

--- CSE335Project1Phase1/Models/courseDictionary.swift
+++ CSE335Project1Phase1/Models/courseDictionary.swift
@@ -15,4 +15,23 @@ class courseDictionary: ObservableObject
     init() {
         
     }
+    
+    /*
+     var className:String? = nil;
+     var roomName:String? = nil;
+     var days = [0, 0, 0, 0, 0, 0, 0];
+     var startTime:Int? = nil;
+     var endTime: Int? = nil;
+     */
+    
+
+    
+    //ADD A COURSE
+    
+    func add_course( _ givenClassName:String, _ givenRoomName:String, _ givenDays:[Int], _ givenStartTime:Int, _ givenEndTime: Int)
+    {
+        list.append(courseRecord(className:givenClassName, roomName:givenRoomName, days:givenDays, startTime:givenStartTime, endTime:givenEndTime));
+        
+        let list = list.sorted(by: {$0.startTime! < $1.startTime!})
+    }
 }
