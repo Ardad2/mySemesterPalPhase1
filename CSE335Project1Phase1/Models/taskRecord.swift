7e1223e0bef5e127f71f125d418685a8cd2f9836
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-03-18 04:25:47 -0700

    added functions to the task record

--- CSE335Project1Phase1/Models/taskRecord.swift
+++ CSE335Project1Phase1/Models/taskRecord.swift
@@ -12,10 +12,10 @@ class taskRecord: Identifiable
     let id = UUID()
     
     var mark:Bool
-    var taskName:String
-    var className:String
-    var dueDate:Date;
-    var dueTime:Int;
+    var taskName:String? = nil;
+    var className:String? = nil;
+    var dueDate:Date? = nil;
+    var dueTime:Int? = nil;
     
     init(taskName:String, className:String, dueDate:Date, dueTime:Int)
     {
@@ -24,6 +24,55 @@ class taskRecord: Identifiable
         self.className = className;
         self.dueDate = dueDate;
         self.dueTime = dueTime;
+    }
+    
+    func change_task(newTaskName:String, newDueDate:Date, newDueTime:Int)
+    {
+        self.taskName = newTaskName;
+        self.dueDate = newDueDate;
+        self.dueTime = newDueTime;
+    }
+    
+    func toggle_mark()
+    {
+        if (self.mark == false)
+        {
+            self.mark = true;
+        }
+        else {
+            self.mark = false;
+        }
+    }
+    
+    func get_task_name() -> String
+    {
+        return self.taskName!
+    }
+    
+    func get_class_name() -> String
+    {
+        return self.className!
+    }
+    
+    func get_due_date() -> Date
+    {
+        return self.dueDate!;
+    }
+    
+    func get_due_date_string() -> String
+    {
+        let ownDate = self.dueDate;
+        let formatter1 = DateFormatter()
+        formatter1.dateStyle = .short;
+        
+        var result = formatter1.string(from: ownDate!);
+        
+        return result;
+    }
+    
+    func get_due_time()
+    {
+        return self.dueTime
     }
     
     
