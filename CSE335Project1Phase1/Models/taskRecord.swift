7e524340eb13f137d5056d741c8d8d3c4134a33c
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-03-18 03:20:58 -0700

    created task record

--- CSE335Project1Phase1/Models/taskRecord.swift
+++ CSE335Project1Phase1/Models/taskRecord.swift
@@ -7,4 +7,27 @@
 
 import Foundation
 
-
+class taskRecord: Identifiable
+{
+    let id = UUID()
+    
+    var mark:Bool
+    var taskName:String
+    var className:String
+    var dueDate:Date;
+    var dueTime:Int;
+    
+    init(mark:Bool, taskName:String, className:String, dueDate:Date, dueTime:Int)
+    {
+        self.mark = mark;
+        self.taskName = taskName;
+        self.className = className;
+        self.dueDate = dueDate;
+        self.dueTime = dueTime;
+    }
+    
+    
+    
+    //Mark, Task Name, courseName, dueDate, duetime,
+    
+}
